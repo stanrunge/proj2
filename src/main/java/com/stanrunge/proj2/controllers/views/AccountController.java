6026a9ed79117499a7e2863ddc3a9ae58135c3eb
Stan <65637274+stanrunge@users.noreply.github.com>
2022-06-15 23:03:06 +0200

    add changePassword button and errorMessage label

--- src/main/java/com/stanrunge/proj2/controllers/views/AccountController.java
+++ src/main/java/com/stanrunge/proj2/controllers/views/AccountController.java
@@ -1,10 +1,14 @@
 package com.stanrunge.proj2.controllers.views;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 
 public class AccountController {
+
+    @FXML
+    private Label errorChangePasswordLabel;
 
     @FXML
     private PasswordField changePasswordField;
@@ -20,5 +24,13 @@ public class AccountController {
 
     @FXML
     private Label usernameLabel;
+
+    @FXML
+    private Button changePasswordButton;
+
+    @FXML
+    private void changePassword() {
+
+    }
 
 }
