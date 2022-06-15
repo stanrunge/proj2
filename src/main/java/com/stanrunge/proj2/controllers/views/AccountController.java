423f0edcbce1d005f853bc1553c9797ef2feaf1d
Stan <stanrunge03@gmail.com>
2022-06-15 22:24:36 +0200

    update account view and controller

--- src/main/java/com/stanrunge/proj2/controllers/views/AccountController.java
+++ src/main/java/com/stanrunge/proj2/controllers/views/AccountController.java
@@ -1,4 +1,24 @@
 package com.stanrunge.proj2.controllers.views;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+
 public class AccountController {
+
+    @FXML
+    private PasswordField changePasswordField;
+
+    @FXML
+    private Label currentPointsLabel;
+
+    @FXML
+    private Label emailLabel;
+
+    @FXML
+    private PasswordField reEnterChangedPasswordField;
+
+    @FXML
+    private Label usernameLabel;
+
 }
