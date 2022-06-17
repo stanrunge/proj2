5389ea438a82b16f61a0b3b46581be82fa371b14
Stan <65637274+stanrunge@users.noreply.github.com>
2022-06-17 13:55:52 +0200

    quick fix scenes enum

--- src/main/java/com/stanrunge/proj2/Scenes.java
+++ src/main/java/com/stanrunge/proj2/Scenes.java
@@ -4,9 +4,6 @@ public enum Scenes {
     LOGIN("/fxml/login.fxml"),
     REGISTER("/fxml/views/RegisterView.fxml");
 
-    private final String path;
-
     Scenes(String s) {
-        this.path = s;
     }
 }
