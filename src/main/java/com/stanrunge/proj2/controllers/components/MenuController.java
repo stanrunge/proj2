c9ad65826d0aeaa0c50723bd082fc71729d5b567
Stan <65637274+stanrunge@users.noreply.github.com>
2022-06-17 13:56:08 +0200

    update account and menu

--- src/main/java/com/stanrunge/proj2/controllers/components/MenuController.java
+++ src/main/java/com/stanrunge/proj2/controllers/components/MenuController.java
@@ -1,4 +1,29 @@
 package com.stanrunge.proj2.controllers.components;
 
+import com.stanrunge.proj2.controllers.views.DashboardController;
+import com.stanrunge.proj2.data.User;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.springframework.context.ApplicationContext;
+
 public class MenuController {
+
+    ApplicationContext applicationContext;
+    User user;
+
+//    void changeView() {
+//        DashboardController dashboardController = applicationContext.getBean(DashboardController.class);
+//        dashboardController.setCurrentUser(user);
+//
+//        Stage stage = (Stage) registerButton.getScene().getWindow();
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/views/DashboardView.fxml"));
+//        loader.setController(dashboardController);
+//        loader.setControllerFactory(applicationContext::getBean);
+//        Parent root = loader.load();
+//        Scene scene = new Scene(root);
+//        stage.setScene(scene);
+//    }
+
 }
--- src/main/java/com/stanrunge/proj2/controllers/views/AccountController.java
+++ src/main/java/com/stanrunge/proj2/controllers/views/AccountController.java
@@ -1,11 +1,23 @@
 package com.stanrunge.proj2.controllers.views;
 
+import com.stanrunge.proj2.data.User;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.stage.FileChooser;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class AccountController {
+
+    User currentUser;
+
+    AccountController(User currentUser) {
+        this.currentUser = currentUser;
+        currentPointsLabel.setText("Current points: " + currentUser.getPoints());
+        emailLabel.setText("Email: " + currentUser.getEmail());
+        usernameLabel.setText("Username: " + currentUser.getUsername());
+    }
 
     @FXML
     private Label errorChangePasswordLabel;
@@ -29,8 +41,21 @@ public class AccountController {
     private Button changePasswordButton;
 
     @FXML
-    private void changePassword() {
+    private FileChooser fileChooser;
 
+    @FXML
+    private void changeProfilePicture() {
+
+    }
+
+    @FXML
+    private void changePassword() {
+        String hashedPassword = new BCryptPasswordEncoder().encode(changePasswordField.getText());
+        currentUser.setHashedPassword(hashedPassword);
+    }
+
+    private boolean isSamePassword() {
+        return changePasswordField.getText().equals(reEnterChangedPasswordField.getText());
     }
 
 }
