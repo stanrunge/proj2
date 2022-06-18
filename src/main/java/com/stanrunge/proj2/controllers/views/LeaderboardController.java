4ec147b6c8237528c09f9aeb201c52865a937a2a
Stan <stanrunge03@gmail.com>
2022-06-18 16:51:50 +0200

    add leaderboard

--- src/main/java/com/stanrunge/proj2/controllers/views/LeaderboardController.java
+++ src/main/java/com/stanrunge/proj2/controllers/views/LeaderboardController.java
@@ -1,7 +1,28 @@
 package com.stanrunge.proj2.controllers.views;
 
+import com.stanrunge.proj2.controllers.data.UserController;
+import com.stanrunge.proj2.data.User;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LeaderboardController {
+
+    UserController userController;
+    Iterable<User> users;
+
+    @FXML
+    TableView<User> leaderboardTable;
+
+    @FXML
+    TableColumn<User, String> usernameColumn;
+
+    @FXML
+    private void initialize() {
+        users = userController.getUsers();
+        leaderboardTable.setItems((ObservableList<User>) users);
+    }
 }
